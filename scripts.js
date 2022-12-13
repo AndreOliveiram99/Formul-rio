93d4fa4f701672f3658addd6a8468954edef84ba
AndreOliveiram99 <andreoliveira.m99@gmail.com>
2022-12-13 19:27:26 -0300

    Forns Pronto

--- scripts.js
+++ scripts.js
@@ -1,15 +1,12 @@
-const form = document.getElementById("form");
+const form = document.getElementById('contact-form');
 
-function handleSubmit(event) {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    
+
     const name = form.elements['name'].value;
     const email = form.elements['email'].value;
     const message = form.elements['message'].value;
-
-    const result = `Nome: ${name}\nE-mail: ${email}\nMensagem: ${message}\n`;
-
+    
+    const result = `Nome: ${name}\nE-mail: ${email}\nMessage: ${message}`;
     alert(result);
-}
-
-form.addEventListener("submit", handleSubmit)
+});
